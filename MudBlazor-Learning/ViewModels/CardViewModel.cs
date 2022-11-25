6c4327a2d8a8ac033253dcfd2dc0fff643ef2c8e
Mateus Castellar <mateuscastellar@outlook.com>
2022-11-25 15:25:32 -0300

    feat: Cards

---
+++ MudBlazor-Learning/ViewModels/CardViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MudBlazor.ViewModels
+{
+    public class CardViewModel
+    {
+        public string? Titulo { get; set; }
+        public string? Imagem { get; set; }
+        public string? Descricao { get; set; }
+    }
+}
