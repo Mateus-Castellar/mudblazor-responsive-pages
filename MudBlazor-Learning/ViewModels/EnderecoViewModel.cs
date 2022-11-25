a55f08936e2dfd48e14427441a17eb116490b841
Mateus Castellar <mateuscastellar@outlook.com>
2022-11-25 14:39:19 -0300

    Add project files.

---
+++ MudBlazor-Learning/ViewModels/EnderecoViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MudBlazor.ViewModels
+{
+    public class EnderecoViewModel
+    {
+        public string? Cep { get; set; }
+        public string? Logradouro { get; set; }
+        public string? Localidade { get; set; }
+        public string? Uf { get; set; }
+        public string? Complemento { get; set; }
+        public string? Numero { get; set; }
+        public string? Bairro { get; set; }
+    }
+}
